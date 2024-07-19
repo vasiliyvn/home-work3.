cf2d01192ae0c4ee66fe3c915ce6aff79ae15112
vasiliyvn <mishadasha1114@gmail.com>
2024-07-19 23:33:27 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+name='Vasiliy'
+print('Name:',name)
+age=36
+print('Age:',age)
+age=age+1
+print('New_age:',age)
+is_student=age>36
+print('Is_Student:',(is_student))
